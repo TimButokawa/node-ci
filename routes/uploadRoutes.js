4a4b7e9f8d7caf6dcfd7ee5ba1e99ff79d5d4b0b
Tim Butorac <tim@timbutorac.com>
2022-03-06 22:13:49 -0800

    file upload

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,29 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+const keys = require('../config/keys');
+const requireLogin = require('../middlewares/requireLogin');
+
+const s3 = new AWS.S3({
+  accessKeyId: keys.accessKeyId,
+  secretAccessKey: keys.secretAccessKey,
+  region: 'us-west-1',
+  signatureVersion: 'v4',
+});
+
+module.exports = app => {
+  // get pre signed url
+  app.get('/api/upload', requireLogin, (req, res) => {
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl('putObject', {
+      Bucket: 'blog-bucket-2022',
+      ContentType: 'image/jpeg',
+      Key: key,
+    }, (err, url) => {
+      res.send({
+        key,
+        url,
+      });
+    });
+  });
+}
